16075e289cc77ce670662adb9142b7e823e3597e
Maxime Ghignet <maxime.ghignet@gmail.com>
2021-12-14 16:43:35 +0100

    basic game (without strikes or spares)

---
+++ src/main/java/com/lmfr/instala/bowling/Game.java
@@ -0,0 +1,15 @@
+package com.lmfr.instala.bowling;
+
+public class Game {
+
+  public Game() {
+  }
+
+  public void roll(int score) {
+
+  }
+
+  public int getScore() {
+    return 0;
+  }
+}
---
+++ src/test/java/com/lmfr/instala/bowling/GameTest.java
@@ -0,0 +1,24 @@
+package com.lmfr.instala.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+
+  @Test
+  void new_game_score_is_zero() {
+    Game game = new Game();
+    assertThat(game.getScore()).isEqualTo(0);
+  }
+
+  @Test
+  void some_rolls() {
+    Game game = new Game();
+    game.roll(3);
+    game.roll(6);
+    game.roll(9);
+    game.roll(0);
+  }
+
+}
